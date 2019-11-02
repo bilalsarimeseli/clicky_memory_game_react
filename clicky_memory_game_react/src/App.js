2699ae1e8cdb66cfa78091c98b679565ae9a7071
Bilal Sarımeşeli <bilalsarimeseli@Bilals-MacBook-Air.local>
2019-11-02 09:42:31 -0700

    gameover fn created

--- clicky_memory_game_react/src/App.js
+++ clicky_memory_game_react/src/App.js
@@ -26,6 +26,42 @@ class App extends Component {
     return true;
   }
 
+  clickCount = id => {
+    this.state.cards.find((o, i) => {
+      if (o.id === id) {
+        if(cards[i].count === 0){
+          cards[i].count = cards[i].count + 1;
+          this.setState({score : this.state.score + 1}, function(){
+            console.log(this.state.score);
+          });
+          this.state.cards.sort(() => Math.random() - 0.5)
+          return true; 
+        } else {
+          this.gameOver();
+        }
+      }
+    });
+  }
+  // Map over this.state.cards and render a cardCard component for each card object
+  render() {
+    return (
+      <Wrapper>
+        <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
+        {this.state.cards.map(card => (
+          <Card
+            clickCount={this.clickCount}
+            id={card.id}
+            key={card.id}
+            image={card.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
+
   
 export default App;
 
