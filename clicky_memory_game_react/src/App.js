203db80848a4d0c3faf51deaafb1e8af811aad47
Bilal Sarımeşeli <bilalsarimeseli@Bilals-Air.attlocal.net>
2019-11-02 15:55:37 -0700

    css properties images modified

--- clicky_memory_game_react/src/App.js
+++ clicky_memory_game_react/src/App.js
@@ -3,6 +3,7 @@ import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
 import cards from "./cards.json";
 import './App.css';
+import Card from './components/Card/Card';
 
 class App extends Component {
   // Setting this.state.cards to the cards json array
