c3b03394e8857f54c70021fdf3ade054dbadf7f9
Bilal Sarımeşeli <bilalsarimeseli@Bilals-Air.attlocal.net>
2019-11-05 19:55:03 -0800

    Updates

---
+++ precache-manifest.49baca7f2755275b8bb33514879a243e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4da96ba0cc7e9a44cfc8756a9d8f2a9",
+    "url": "/clicky_memory_game_react/index.html"
+  },
+  {
+    "revision": "fe55c73576a09c2ee006",
+    "url": "/clicky_memory_game_react/static/css/main.dde4086a.chunk.css"
+  },
+  {
+    "revision": "793a82af17b8ebb57507",
+    "url": "/clicky_memory_game_react/static/js/2.df692479.chunk.js"
+  },
+  {
+    "revision": "fe55c73576a09c2ee006",
+    "url": "/clicky_memory_game_react/static/js/main.d87b5a73.chunk.js"
+  },
+  {
+    "revision": "727ef4a8d5d1d550ebd6",
+    "url": "/clicky_memory_game_react/static/js/runtime-main.bf467f24.js"
+  }
+]);
